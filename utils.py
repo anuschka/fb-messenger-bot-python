877abe118c7a1c6b438b747dfd4b36c968cc45df
Ana Sustič <ana_sustic@ztm.local>
2018-01-31 13:42:12 +0100

    Added wit.ai to the application

---
+++ utils.py
@@ -0,0 +1,19 @@
+from wit import Wit
+
+access_token = "IHT4ASS62NJ4C6A3GI42CVSSNC5A4KYJ"
+
+client = Wit(access_token = access_token)
+
+def wit_response(message_text):
+    resp = client.message(message_text)
+    entity = None
+    value = None
+    try:
+        entity = list(resp['entities'])[0]
+        value = resp['entities'][entity][0]['value']
+    except:
+        pass
+    return (entity,value)
+
+
+print(wit_response('I want sport news'))
