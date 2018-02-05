5c2f9da4c2cde83f78af686a80c4b730221b0263
anuschka <sustic@gmail.com>
2018-02-05 19:37:08 +0100

    Adds initwelcome.sh

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def received_message(event):
         elif message_text == 'file' or message_text == 'File':
             send_file_message(sender_id)
 
-        elif message_text == 'audio' or message_text == 'audio':
+        elif message_text == 'audio' or message_text == 'Audio':
             send_audio_message(sender_id)
 
         elif message_text == 'video' or message_text == 'Video':
